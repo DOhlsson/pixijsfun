fd8188ddeac342b71bbf0717a250f13bb5dc38a9
Daniel Ohlsson <daniel.ohlsson@sensative.com>
2017-09-24 00:47:41 +0200

    commit

--- src/index.js
+++ src/index.js
@@ -25,12 +25,23 @@ let users = [];
 
 io.on('connection', function(socket) {
   socket.on('move', function(msg) {
-    x = msg.x;
-    y = msg.y;
+    users[socket.id] = {
+      x: msg.x,
+      y: msg.y
+    };
     io.emit('coords', {y, x, id: socket.id});
     console.log(socket.id + ' sent ' + JSON.stringify(msg, null, 2));
   });
+  socket.on('jump', function() {
+    console.log('we got jump');
+  });
+
+  socket.on('disconnect', function() {
+    console.log('disconnect', socket.id);
+  });
 });
-io.on('disconnect', function(socket) {
-  console.log('disconnect', socket.id);
-});
+
+function gameLoop() {
+}
+
+setInterval(gameLoop, 100);
