8db6295274c6668d8c7709496cf16983711831f5
Robin Karlsson <s.r.karlsson@gmail.com>
2017-09-24 02:20:27 +0200

    gulp watch for js changes and restart server

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,32 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    spawn = require('child_process').spawn,
+    node;
 var jshint = require('gulp-jshint');
 
+var sources = ['src/*.js', 'src/static/js/*.js', '!src/static/js/socket.io.js', '!src/static/js/pixi.js'];
+
+gulp.task('server', function() {
+  if (node) node.kill();
+  node = spawn('node', ['src/index.js'], {stdio: 'inherit'});
+  node.on('close', function (code) {
+    if (code === 8) {
+      gulp.log('Error detected, waiting for changes...');
+    }
+  });
+});
+
 gulp.task('lint', function() {
-  return gulp.src(['src/*.js', 'src/static/js/*.js', '!src/static/js/socket.io.js', '!src/static/js/pixi.js'])
+  return gulp.src(sources)
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
 });
 
-gulp.task('default', ['lint']);
+gulp.task('watch', function() {
+  gulp.watch(sources, ['lint', 'server']);
+});
+
+process.on('exit', function() {
+  if (node) node.kill();
+});
+
+gulp.task('default', ['lint', 'watch', 'server']);
